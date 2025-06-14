2f3a41bd50b2551051411dac974530c139c586ff
Tharun S <tharunsuresh2004@gmail.com>
2025-06-14 14:24:50 +0530

    Initial commit

---
+++ Movieapp/frontend/src/contexts/MovieContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useState, useEffect, useContext } from "react";
+
+const MovieContext = createContext();
+
+export const useMovieContext = () => useContext(MovieContext);
+
+export const MovieProvider = ({ children }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+    if (storedFavs) {
+      setFavorites(JSON.parse(storedFavs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addToFavorites = (movie) => {
+    setFavorites((prev) => [...prev, movie]);
+  };
+
+  const removeFavorites = (movieId) => {
+    setFavorites((prev) => prev.filter((movie) => movie.id !== movieId));
+  };
+
+  const isFavorite = (movieId) => {
+    return favorites.some((movie) => movie.id === movieId);
+  };
+
+  return (
+    <MovieContext.Provider
+      value={{
+        favorites,
+        setFavorites,
+        addToFavorites,
+        removeFavorites,
+        isFavorite,
+      }}
+    >
+      {children}
+    </MovieContext.Provider>
+  );
+};
